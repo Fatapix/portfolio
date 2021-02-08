c434f5c27c4d219e29ce87e2064308fecbf06751
fatapix <khiratnohadarocha@gmail.com>
2021-02-08 11:01:32 +0100

    feat(app): Add Home section

---
+++ app/components/sections/HomeSection.tsx
@@ -0,0 +1,27 @@
+export default function HomeSection() {
+    return (
+        <section className="section home-section">
+            <div className="background-filter"></div>
+            
+            <span className="tags top-tags">&nbsp;&nbsp;&nbsp;&lt;body&gt;</span> 
+
+            <div className="content">
+                <div className="container">
+                    <div className="subtitle">Noha Khirat</div>
+                    <div className="title">
+                        Développeur Web
+                    </div>
+                    <p>Full-Stack - Typescript NodeJS</p>
+                </div>
+            </div>
+
+            <span className="tags bottom-tags">
+                &nbsp;&nbsp;&nbsp;&lt;/body&gt;
+                <br/>
+                &lt;/html&gt;
+            </span>
+
+            <div className="scroll-icon"></div>
+        </section>
+    )
+}
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 
 import Layout from '../components/Layout'
+import HomeSection from '../components/sections/HomeSection'
 
 export default function Home() {
   return (
     <Layout title="Portfolio - Développeur Web">
-
+      <HomeSection />
     </Layout>
   )
 }
