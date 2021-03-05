0f62dac06f1523a17b0b82af9e3e35c6b6c3140d
fatapix <khiratnohadarocha@gmail.com>
2021-03-05 09:02:56 +0100

    Install strapi

---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '457f1931-fd1d-40f3-9ba9-6c9ae5cc558b'
+};
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,15 +2,21 @@ export default function Header() {
     return (
         <header className="header">
             <nav className="socials-bar">
-                <a href="https://github.com/Fatapix" title="GitHub">
-                    <img src="/icons/icon-github.svg"/>
-                </a>
-                <a href="#">
-                    <img src="/icons/icon-mail.svg" title="Mail"/>
-                </a>
-                <a href="https://www.linkedin.com/in/noha-khirat-1a7bb91a9/?originalSubdomain=fr">
-                    <img src="/icons/icon-linkedin.svg" title="LinkedIn"/>
-                </a>
+                <div className="contacts-container">
+                    <label className="mail">contact@khirat-noha.fr</label>
+                    <label className="mail">06 72 53 07 47</label>
+                </div>
+                <div className="links-container">
+                    <a href="https://github.com/Fatapix" title="GitHub">
+                        <img src="/icons/icon-github.svg"/>
+                    </a>
+                    <a href="#">
+                        <img src="/icons/icon-mail.svg" title="Mail"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/noha-khirat-1a7bb91a9/?originalSubdomain=fr">
+                        <img src="/icons/icon-linkedin.svg" title="LinkedIn"/>
+                    </a>
+                </div>
             </nav>
             <div className="menu-icon">
                 <span></span>
@@ -18,7 +24,7 @@ export default function Header() {
             <nav className="navigation-bar">
                 <ul className="links">
                     <li className="item active">
-                        <a className="link" href="#home-section">Acceuil</a>
+                        <a className="link" href="#home-section">Accueil</a>
                     </li>
                     <li className="item">
                         <a className="link" href="#about-section">A propos</a>
