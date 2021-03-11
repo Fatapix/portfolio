20ff4d67680dcb216ff9bebe202390990954d789
fatapix <khiratnohadarocha@gmail.com>
2021-03-11 13:37:29 +0100

    feat(sections): create PortfolioSection and add in website

---
+++ app/components/sections/PortfolioSection.tsx
@@ -0,0 +1,7 @@
+export default function PortfolioSection() {
+    return (
+        <section className="portfolio-section">
+            <h1>TODO</h1>
+        </section>
+    )
+}
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -3,12 +3,14 @@ import Head from 'next/head'
 import Layout from '../components/Layout'
 import HomeSection from '../components/sections/HomeSection'
 import AboutSection from '../components/sections/AboutSection'
+import PortfolioSection from '../components/sections/PortfolioSection'
 
 export default function Home() {
   return (
     <Layout title="Portfolio - Développeur Web">
       <HomeSection />
       <AboutSection />
+      <PortfolioSection />
     </Layout>
   )
 }
