f307f543624777d2f9245f38eafa4c3021bfdeb2
fatapix <khiratnohadarocha@gmail.com>
2021-03-11 13:34:10 +0100

    feat(index): Import and add AboutSection component in website

--- app/components/sections/AboutSection.tsx
+++ app/components/sections/AboutSection.tsx
@@ -1,7 +1,7 @@
 export default function AboutSection() {
     return (
     <section className="about-section">
-        
+        <h1>TODO</h1>
     </section>
     )
 }
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,11 +2,13 @@ import Head from 'next/head'
 
 import Layout from '../components/Layout'
 import HomeSection from '../components/sections/HomeSection'
+import AboutSection from '../components/sections/AboutSection'
 
 export default function Home() {
   return (
     <Layout title="Portfolio - Développeur Web">
       <HomeSection />
+      <AboutSection />
     </Layout>
   )
 }
